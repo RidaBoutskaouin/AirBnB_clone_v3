381385899f0596275ce5a05c935fc3f8d2e0ce54
kostum-kirakos <lkoustim.mohmaed@yahoo.com>
2024-02-05 06:47:58 -0800

    fix style and output

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,13 +18,13 @@ def status():
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
+def number_of_objs():
     stats = {}
     classes = [Amenity, City, Place, Review, State, User]
     names = ["amenities", "cities", "places", "reviews", "states", "users"]
     for i in range(len(classes)):
         cls = classes[i]
         name = names[i]
-        stats[name] = cls.count()
+        stats[name] = cls.count(cls)
 
     return jsonify(stats)
