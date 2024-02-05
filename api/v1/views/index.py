993adff9b42b9ccc787d36a5fb12b1782df841f5
kostum-kirakos <lkoustim.mohmaed@yahoo.com>
2024-02-05 06:44:40 -0800

    fix style and output

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,18 +10,21 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """status"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    states = {}
+    stats = {}
     classes = [Amenity, City, Place, Review, State, User]
     names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    for i in range(len(classes)):
+        cls = classes[i]
+        name = names[i]
+        stats[name] = cls.count()
 
-    for cls in classes:
-        states[cls] = storage.count(cls)
-    return jsonify(states)
-        
+    return jsonify(stats)
